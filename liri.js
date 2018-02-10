c7ba4574d6adfbfaf16d9a0b0444df4688b6e421
zachha <zachha@gmail.com>
2018-02-10 13:55:46 -0500

    liri.js updated, comments added

--- liri.js
+++ liri.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const request = require('request');
 const keys = require('keys.js');
+const command = process.argv[3];
+const specifiedMedia = process.argv[4];
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 console.log(spotify);
 
+
 /*
+// LOOK UP VS DEBUG AND WORK ON GETTING USED TO DEBUGGING!
 //show last 20 tweets and when they were created (twitter API)
 function tweets(a) {
 
