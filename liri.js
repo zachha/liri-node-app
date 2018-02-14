98b1de0baa197bc556033fd9942387525506a0a2
zachha <zachha@gmail.com>
2018-02-13 21:35:57 -0500

    omdb + request functions almost complete

--- liri.js
+++ liri.js
@@ -10,8 +10,10 @@ let media = process.argv.splice(3).join(" ");
 //Calling in API keys
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
 //console.log(command);
 //console.log(media);
+
 //switch takes the user's command and applies the relavent function and package for each
 switch(command) {
     case "my-tweets":
@@ -23,7 +25,7 @@ switch(command) {
     break;
 
     case "movie-this":
-    movie();
+    movie(media);
     break;
 
     case "do-what-it-says":
@@ -33,6 +35,7 @@ switch(command) {
 
 
 // LOOK UP VS DEBUG AND WORK ON GETTING USED TO DEBUGGING!
+
 //show last 20 tweets and when they were created (twitter package)
 function tweets() {
     
@@ -48,7 +51,7 @@ function tweets() {
 }
 
 //show song info: artist(s), song name, spotify preview link, album song is from
-// if no song provided, default to Ace of Bass' 'The Sign' (Spotify package)
+// if no song provided, default to Ace of Base's 'The Sign' (Spotify package)
 function song(media) {
     if (media === "") {
         media = "The Sign"
@@ -68,13 +71,34 @@ function song(media) {
     console.log(data.tracks.items[0].album.name);
     });
 }
-/*
+
 //show movie info: movie title, year, IMDB rating, Rotten Tomatoes rating, Country of movie, Movie language, Plot Summary, Actors in movie
 //if no input, defaults to 'Mr. Nobody' (OMDB package)
-function movie() {
+function movie(media) {
+    const queryUrl = 'http://www.omdbapi.com/?t=' + media + '&y=&plot=short&apikey=trilogy';
+    console.log(queryUrl);
+
+    request(queryUrl, function (error, response, body) {
+
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            JSON.parse(body);
+            console.log("\n", 'Title: ' + JSON.parse(body).Title);
+            console.log("\n", 'Release Year: ' + JSON.parse(body).Year);
+            console.log("\n", 'IMDB Rating: ' + JSON.parse(body).imdbRating);
+            console.log("\n", 'Rotten Tomatoes Rating: ' + JSON.parse(body).Ratings[2]);
+            console.log("\n", 'Country of Origin: ' + JSON.parse(body).Country);
+            console.log("\n", 'Language: ' + JSON.parse(body).Language);
+            console.log("\n", 'Plot: ' + JSON.parse(body).Plot);
+            console.log("\n", 'Actors: ' + JSON.parse(body).Actors);
+        }
+    });
 
 }
-
+/*
 // using fs Node package, takes text inside random.txt and then use it to call one of LIRI's commands (should run spotify-this-song for "I Want It That Way" as follows the text in random.txt, can also make changes to text to test feature for other commands)
 function readAndDo () {
 
